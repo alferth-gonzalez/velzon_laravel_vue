6977bf3888682fe420c835a7160d7a132c4a95e4
ALFERTH GONZALEZ <derinjos2436@hotmail.com>
2025-09-04 23:48:32 -0500

    Init

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 4000,
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@assets': '/resources/', // Update this with the correct path to your images
+            '@favicon': '/resources/images/', // Update this with the correct path to your images
+        },
+    },
+});
